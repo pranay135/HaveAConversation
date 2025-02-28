c01a670e0301dc06706590cf0b4acee0767157dd
pranay135 <pranaydevangan880@gmail.com>
2025-02-28 09:20:25 +0530

    28-02-25 Today's Commit

---
+++ src/main/java/org/example/model/Message.java
@@ -0,0 +1,14 @@
+package org.example.model;
+
+import lombok.*;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@ToString
+public class Message {
+
+    private String name;
+    private String content;
+}
